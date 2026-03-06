d5bd05c598661bb72a7dc6f7c4f8e4090d714d08
Lisa Wang <lisa.w@koalr.com>
2026-03-06 10:41:02 -0700

    test: Fix CSS layout issues on mobile

--- src/components/PRChange32.tsx
+++ src/components/PRChange32.tsx
@@ -1,1 +1,2 @@
 // Change 8 for PR 2 - Fix CSS layout issues on mobile
+// Change 3 for PR 5 - Fix CSS layout issues on mobile
--- src/components/PRChange49.tsx
+++ src/components/PRChange49.tsx
@@ -1,1 +1,2 @@
 // Change 6 for PR 2 - Fix CSS layout issues on mobile
+// Change 1 for PR 5 - Fix CSS layout issues on mobile
